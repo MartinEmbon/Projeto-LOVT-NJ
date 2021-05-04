27dc0133f1d0ece7e0f13921320be4101c39a72a
Martincho <martinembon@hotmail.com>
2021-05-03 21:18:13 -0300

    contact page adicionado

--- app.js
+++ app.js
@@ -5,3 +5,32 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0]
 toggleButton.addEventListener("click",()=>{
     navbarLinks.classList.toggle("active")
 })
+
+const form = document.getElementById("form")
+const fname = document.getElementById("fname")
+const lname = document.getElementById("lname")
+const email = document.getElementById("email")
+const msg = document.getElementById("mensaje")
+const errorElement = document.getElementById("error")
+
+form.addEventListener("submit",(e)=>{
+    let errores = []
+    if (fname.value==="" || fname.value==null){
+        errores.push("Nombre es obligatorio")
+    }
+    if (lname.value==="" || lname.value==null){
+        errores.push("Apellido es obligatorio")
+    }
+    if (email.value==="" || email.value==null){
+        errores.push("Campo no debe permanecer vacio")
+    }
+    if (msg.value==="" || msg.value==null){
+        errores.push("Campo Mensaje es obligatorio")
+    }
+    if (errores.length>0){
+        e.preventDefault();
+        errorElement.innerText = errores.join(", ")
+
+    }
+})
+
